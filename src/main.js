2b93b53de2b2d8a94065302597c0605675831c31
Andres Felipe <juandavidquimbayo99@gmail.com>
2022-07-01 18:10:12 -0500

    funcionamiento de la pagina y estilos

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 /*
   Aquí va tu JavaScript
 */
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+
+
+start_btn.onclick = () => {
+  info_box.classList.add("activeInfo");
+}
+exit_btn.onclick = () => {
+  info_box.classList.remove("activeInfo");
+  continue_btn.onclick = () => {
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
+
+  }
+}
---
+++ src/question.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Para que sirve HTML?",
+        answer: "Lenguaje de marcado de hipertexto",
+        options: [
+            "Que es procesador de hipertexto",
+            "Que es lenguaje de marcado de hipertexto",
+            " Cual no es un lenguaje de programación",
+            "Javascript es lo mismo que java"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "Que significa css",
+        answer: "Hoja estilo cascada",
+        options: [
+            "Hoja de estilo común",
+            "hoja de estilo colorido",
+            "Hoja de estilo de computadora",
+            "Hoja estilo cascada"
+        ]
+
+    },
+    {
+        numb: 3,
+        question: "Que es PHP",
+        answer: "Procesador de hipertexto",
+        options: [
+            "Procesador de hipertexto",
+            "Programación de hipertexto",
+            "Preprogramación de hipertexto",
+            "Preprocesador de texto de inicio"
+
+        ]
+    },
+    {
+
+        numb: 4,
+        question: "Que significa sql",
+        answer: "lenguaje de consulta estructurado",
+        options: [
+            "lenguaje de preguntas con estilo",
+            "lenguaje de consulta de hoja de estilo",
+            "Enunciado pregunta lenguaje",
+            "Lenguaje de consulta estructurado"
+        ]
+    },
+    {
+
+        numb: 5,
+        question: "Que significa XML",
+        answer: "múltiples lenguajes extensibles",
+        options: [
+            "lenguaje de marcado extensible",
+            "múltiples lenguajes extensibles",
+            "lenguaje de programa múltiple adicional",
+            "examinar varios idiomas"
+        ]
+    },
+];
+
